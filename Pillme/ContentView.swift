31c9d43ae4ce3bacd7444294923cf68e6c9a2a58
yongtae.kwon <yongtae.kwon@navercorp.com>
2021-09-13 12:31:25 +0900

    headerView Offset에 따라 background opacity 조정

--- Pillme/ContentView.swift
+++ Pillme/ContentView.swift
@@ -20,66 +20,88 @@ struct ContentView: View {
         }
     }
     
+    var headerViewOpacity: Double {
+        switch contentOffset {
+        case ...0:
+            return 0
+        case 0...100:
+            return Double(contentOffset) / 100.0
+        default:
+            return 1
+        }
+    }
+    
     var body: some View {
         NavigationView {
             ZStack {
                 Color.mainColor.ignoresSafeArea()
                 ZStack {
                     ScrollView(.vertical) {
-                        VStack {
-                            VStack(alignment: .leading, spacing: 0) {
-                                Text("권용태님")
-                                Text("점심 맛있게 드셨나요?")
-                                Text("식사 후에 ") + Text("오메가3 ").foregroundColor(Color.tintColor).fontWeight(.heavy) + Text("잊지마세요!")
+                        ZStack {
+                            VStack {
+                                VStack(alignment: .leading, spacing: 0) {
+                                    Text("권용태님")
+                                    Text("점심 맛있게 드셨나요?")
+                                    Text("식사 후에 ") + Text("오메가3 ").foregroundColor(Color.tintColor).fontWeight(.heavy) + Text("잊지마세요!")
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(EdgeInsets(top: Layout.headerHeight, leading: 20, bottom: 10, trailing: 20))
+                                .font(.system(size: 27, weight: .bold, design: .default))
+                                .foregroundColor(.white)
+                                
+                                VStack(alignment: .leading, spacing: 0) {
+                                    ZStack(alignment: .leading) {
+                                        Color.white.opacity(0.1)
+                                        VStack(alignment: .leading, spacing: 10) {
+                                            Text("잊은 약❓").foregroundColor(.white).font(.title2).fontWeight(.bold)
+                                            HStack {
+                                                PillNameButtonView("아연")
+                                            }
+                                        }.padding(20)
+                                    }.cornerRadius(20)
+                                    Spacer(minLength: 20)
+                                    ZStack(alignment: .leading) {
+                                        Color.white.opacity(0.1)
+                                        VStack(alignment: .leading, spacing: 10) {
+                                            Text("먹은 약").foregroundColor(.white).font(.title2).fontWeight(.bold)
+                                            HStack {
+                                                PillNameButtonView("유산균")
+                                                PillNameButtonView("아르기닌")
+                                                PillNameButtonView("칼슘")
+                                            }
+                                        }.padding(20)
+                                    }.cornerRadius(20)
+                                }.frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(20)
+                                
+                                ZStack {
+                                    Color.white.opacity(0.1)
+                                    VStack(alignment: .leading, spacing: 10) {
+                                        Text("월간 복용도").foregroundColor(.white).font(.title2).fontWeight(.bold)
+                                        CalendarView(width: UIScreen.main.bounds.size.width - 80, fontColor: .white)
+                                    }.padding(20)
+                                }.cornerRadius(20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(20)
+                                
+                                Spacer()
                             }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(EdgeInsets(top: Layout.headerHeight, leading: 20, bottom: 10, trailing: 20))
-                            .font(.system(size: 27, weight: .bold, design: .default))
-                            .foregroundColor(.white)
                             
-                            VStack(alignment: .leading, spacing: 0) {
-                                ZStack(alignment: .leading) {
-                                    Color.white.opacity(0.1)
-                                    VStack(alignment: .leading, spacing: 10) {
-                                        Text("잊은 약❓").foregroundColor(.white).font(.title2).fontWeight(.bold)
-                                        HStack {
-                                            PillNameButtonView("아연")
-                                        }
-                                    }.padding(20)
-                                }.cornerRadius(20)
-                                Spacer(minLength: 20)
-                                ZStack(alignment: .leading) {
-                                    Color.white.opacity(0.1)
-                                    VStack(alignment: .leading, spacing: 10) {
-                                        Text("먹은 약").foregroundColor(.white).font(.title2).fontWeight(.bold)
-                                        HStack {
-                                            PillNameButtonView("유산균")
-                                            PillNameButtonView("아르기닌")
-                                            PillNameButtonView("칼슘")
-                                        }
-                                    }.padding(20)
-                                }.cornerRadius(20)
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(20)
+                            GeometryReader { geo in
+                                let offset = geo.frame(in: .named("scroll")).minY
+                                Color.clear.preference(key: ScrollViewOffsetPreferenceKey.self, value: -offset)
+                            }
                             
-                            ZStack {
-                                Color.white.opacity(0.1)
-                                VStack(alignment: .leading, spacing: 10) {
-                                    Text("월간 복용도").foregroundColor(.white).font(.title2).fontWeight(.bold)
-                                    CalendarView(width: UIScreen.main.bounds.size.width - 80, fontColor: .white)
-//
-                                }.padding(20)
-                            }.cornerRadius(20)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(20)
-                            
-                            Spacer()
                         }
+                    }
+                    .coordinateSpace(name: "scroll")
+                    .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+                        self.contentOffset = value
                     }
                     
                     GeometryReader { geo in
                         ZStack {
-                           // Blur(style: .dark)
+                            Blur(style: .dark).opacity(headerViewOpacity)
                             VStack {
                                 HStack {
                                     Text("💊 PillMe")
@@ -112,6 +134,14 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue = CGFloat.zero
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value += nextValue()
     }
 }
 
